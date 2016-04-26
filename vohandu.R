6564d073f35d27a19ac222886a2a001d3dfdb4a8
Madis <reinumag@gmail.com>
2016-04-26 16:05:56 -0400

    Manksu tuli segama

--- vohandu.R
+++ vohandu.R
@@ -9,5 +9,15 @@ final.table.df        <- final.table.df[-c(10, 9, 7)] # mõnda veergu pole vaja
 split.url  <- "http://www.vohandumaraton.ee/maraton/index2016.php?act=vahed"
 tables <- readHTMLTable(split.url)
 split.table.df <- tables[[2]]
+split.table.df <- split.table.df[-3] # drop class
 
 compl.df <- merge(final.table.df, split.table.df, by = "Stardinr")
+
+compl.df$Koht <- ifelse(compl.df$Koht == "", NA, compl.df$Koht)
+compl.df$`Koht klassis` <- ifelse(compl.df$`Koht klassis`  == "", NA, compl.df$`Koht klassis`)
+compl.df$Jrk <- ifelse(compl.df$Jrk  == "", NA, compl.df$Jrk)
+
+compl.df <- compl.df[order(compl.df$Jrk),]
+
+names.df <- compl.df[c(1,6)]
+compl.df$Nimed <- substr(compl.df$Nimed, 1, 30) 

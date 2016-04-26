6c7f64851b969f8f40dc1e82f03c2da3adbeaafc
Madis <reinumag@gmail.com>
2016-04-26 12:22:19 -0400

    2.

--- vohandu.R
+++ vohandu.R
@@ -3,7 +3,11 @@ final.url <- "http://www.vohandumaraton.ee/maraton/index2016.php?act=finish"
 tables <- readHTMLTable(final.url)
 
 final.table.df <- tables[[2]]
-names(final.table.df) <- c("Koht","Koht klassis" ,"Klass", "Kood", "Riik", "Nimed", "Tiim", "Aeg", "Kaotus", "Diplom")
+names(final.table.df) <- c("Koht","Koht klassis" ,"Klass", "Stardinr", "Riik", "Nimed", "Tiim", "Aeg", "Kaotus", "Diplom")
 final.table.df        <- final.table.df[-c(10, 9, 7)] # mõnda veergu pole vaja
 
-split.url  <- 
+split.url  <- "http://www.vohandumaraton.ee/maraton/index2016.php?act=vahed"
+tables <- readHTMLTable(split.url)
+split.table.df <- tables[[2]]
+
+compl.df <- merge(final.table.df, split.table.df, by = "Stardinr")
